ca86351436cf0acba82fb9fdb6668aebe64c4339
broliang <18706703437@163.com>
2019-05-28 11:53:58 +0800

    Fix typo

--- training_journal/write_journal.py
+++ training_journal/write_journal.py
@@ -5,7 +5,7 @@ from string import Template
 import datetime
 
 replacements = {}
-to_be_replaced = ['date', 'exercise1', 'warm_set_name1', 'warm_reps1', 'work_set_name1', 'work_reps1', 'exercise2', 'work_set_name2', 'warm_set_name2', 'warm_reps2', 'work_set_name2', 'work_reps2']
+to_be_replaced = ['date', 'exercise1', 'warm_set_name1', 'warm_reps1', 'work_set_name1', 'work_reps1', 'exercise2', 'warm_set_name2', 'warm_reps2', 'work_set_name2', 'work_reps2']
 to_replace = [datetime.datetime.now().strftime("%A, %m/%d/%Y %H:%M"), *sys.argv[1:]]
 replacements = dict(zip(to_be_replaced, to_replace))
 
